8d8fbf05e054f1e9ea54778f8ffea134158e24d0
alank <alankan976@gmail.com>
2020-04-19 23:53:00 +0800

    truncate db dependency for testing

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,16 @@ use todo::{TodoRepositoryImpl, TodoServiceImpl};
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
-    std::env::set_var("DATABASE_URL", "postgresql://todo:todo@localhost:5432/todo");
+    // std::env::set_var("DATABASE_URL", "postgresql://todo:todo@localhost:5432/todo");
     std::env::set_var("RUST_LOG", "debug,my_errors=debug,actix_web=info");
     std::env::set_var("RUST_BACKTRACE", "1");
     let url = std::env::var("PORT")
-        .map(|p|format!("127.0.0.1:{}", p))
+        .map(|p| format!("127.0.0.1:{}", p))
         .unwrap();
     env_logger::init();
 
     let app_data = Arc::new(Components::new().await);
-    
+
     HttpServer::new(move || {
         App::new()
             .data(Arc::clone(&app_data))
